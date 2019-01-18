05eaf28ecd9337f9aa8b5a7e59240ce224ae03ca
Jerome Parent-Levesque <jerome-pl2@hotmail.ca>
2019-01-18 10:59:33 -0500

    Remove service worker to allow faster bug fixing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import './index.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.register();
