a13b67da83fbdee1cf9a81b5812527ef15255a71
Jerome Parent-Levesque <jerome-pl2@hotmail.ca>
2019-01-17 23:58:00 -0500

    First build

---
+++ precache-manifest.ab4413c135df6565a0e5fd61b333b934.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "72701db7d039f755b2279e0113f9cb43",
+    "url": "/static/media/jeromepl-resume.72701db7.pdf"
+  },
+  {
+    "revision": "f4520ea9dd956496cef1",
+    "url": "/static/css/main.1f7706f7.chunk.css"
+  },
+  {
+    "revision": "67863e9987db9f0d91e7a5a6b1d50abd",
+    "url": "/static/media/name.67863e99.svg"
+  },
+  {
+    "revision": "a37024f442c335cfb9d4",
+    "url": "/static/js/1.a37024f4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0d1c4ad38ec54567c795c5f93e904fe1",
+    "url": "/static/media/profile.0d1c4ad3.png"
+  },
+  {
+    "revision": "87be83f91d0f68e9a3b540f775640385",
+    "url": "/static/media/locomotion.87be83f9.mp4"
+  },
+  {
+    "revision": "f4520ea9dd956496cef1",
+    "url": "/static/js/main.f4520ea9.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a37024f442c335cfb9d4",
+    "url": "/static/css/1.ec041387.chunk.css"
+  },
+  {
+    "revision": "d0702e65e96e35b9331d6e8845c55c20",
+    "url": "/index.html"
+  }
+];
