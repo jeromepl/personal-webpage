d99ff9eb6e2d2c7500e7c2ac383eeca85d12e67e
Jerome Parent-Levesque <jerome-pl2@hotmail.ca>
2019-01-18 00:37:47 -0500

    Add deploy command

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Simply run `npm install` once. Then run in dev mode with `npm start`.
 
 ## Deployment instructions
 
-Run `npm run build`, then commit the build. Finally, run `git subtree push --prefix build origin gh-pages` to deploy on github pages.
+Run `npm run deploy`.
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ab4413c135df6565a0e5fd61b333b934.js"
+  "/personal-webpage/precache-manifest.2b30efa9bd06a012990c41eef2d6702b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/personal-webpage/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
